5faa5b500e5b56c624a5ea3aa5072d55e1e0e19f
TeamGLaTOS <daniel.martinez@email.wsu.edu>
2014-01-31 14:56:28 -0800

    homework1_additions
    started homework 1

---
+++ Doc/Deliverables/homwork01/homework01/INItargetReader.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
+public class INItargetReader
+{
+    string header = "";     // this pertains to the [Target]
+    string name = "";       // this identifies name of target
+    double x = 0;           // x cordinate
+    double y = 0;           // y cordinate
+    double z = 0;           // z cordinate
+    bool friend = false;    // determines if a target is friend or foe
+    double points = 0;      // used to keep track of what target is valued
+    int flashRate = 0;      // idicates how long the target is visible
+    public void INITargetReader.Parse(file rfile)
+    {
+        string line = "";
+        ArrayList myArray = new ArrayList();
+        
+        while (line != null)
+        {
+            line = rfile.Console.ReadLine();
+            if (line != null)
+            {
+                int i = 0;
+                for (i; i <= 7; i++)
+                {
+                    myArray.Add(line);
+                    i++;
+                    line = rfile.ReadLine();
+                }
+                i = 0;
+                // at this point we need to strip off after '=' 
+                // we need to creat an object of our class type to use setters 
+            }
+        }
+    }
+    void set(string n, double xcor, double ycor, double zcor, bool target, double p, int f)
+    {
+        name = n;
+        x = xcor;
+        y = ycor;
+        z = zcor;
+        friend = target;
+        points = p;
+        flashRate = f;
+    }
+}
+
---
+++ Doc/Deliverables/homwork01/homework01/homework01/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
+
+namespace HelloWorld
+{
+    class Hello
+    {
+        
+        static void Main(string[] args)
+        {
+            
+            string line = "";
+            // will store the file from StreamReader's argument in rfile
+            System.IO.StreamReader rfile = null;//new System.IO.StreamReader(args[0]);
+            //check to see if there is an argument
+            if (args.Length > 0)
+            {
+                rfile = new System.IO.StreamReader(args[0]);
+            }
+
+            // will read a line of rfile and display it out to console until we reach the EOF(null)
+            TextWriter wfile = new StreamWriter("C:/Users/Daniel/Desktop/SnD/date.txt");
+            while ((line = rfile.ReadLine()) != null)
+            {
+                wfile.WriteLine(line);  
+                Console.WriteLine(line);
+            }
+            wfile.Close();
+
+            //close the file we just used
+            rfile.Close();
+            
+        }
+    }
+
+}
+
